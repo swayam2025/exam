5bb91013c21a9a02a9992c7cc39aeef188d66db3
swayam sharma <karmveersharma6121@gmail.com>
2024-03-13 14:33:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+const PORT = 4444;
+const path = require("path")
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use("/",require("./routes/blog"))
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/Blogg')
+.then(()=>{
+    app.listen(PORT, () => {
+        console.log("http://localhost:" + PORT);
+    })
+})
